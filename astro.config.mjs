33e789d071e99af5c4f044a41a82b35f8db6728e
Emilio Quintero <144851614+MilioQ@users.noreply.github.com>
2024-04-22 18:04:43 -0700

    Cambios en las pantallas y agregamos Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import expressiveCode from "astro-expressive-code";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [preact(), expressiveCode(), tailwind()]
 });
